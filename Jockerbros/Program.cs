0b4328c116e980a6d766f3c32ed5bbe5f518767d
Your Name <you@example.com>
2020-03-26 21:50:19 +0100

    Gamer and RoundOfGames

--- Jockerbros/Program.cs
+++ Jockerbros/Program.cs
@@ -8,6 +8,7 @@ namespace Jockerbros
     {
         static void Main(string[] args)
         {
+            // მოთამაშეების განსაზღვრა
             var gamers = new List<Gamer>
             {
                 new Gamer("Rati Devdariani"),
@@ -16,22 +17,39 @@ namespace Jockerbros
                 new Gamer("Anuki Devdariani")
             };
 
-
+            // კარტის დასტის შექმნა
             DeckOfCardCreator deckOfCardCreator = new DeckOfCardCreator();
+            // კარტის დასტა
             List<Card> deckOfCard = new List<Card>();
             deckOfCard.AddRange(deckOfCardCreator.CreateDeckOfCards());
-            foreach (Card item in deckOfCard)
+            //foreach (Card item in deckOfCard)
+            //{
+            //    Console.WriteLine("{0}", item);
+            //}
+
+            // კარტის დასტის აჩეხვა
+            var mixDckOfCard = new MixDeckOfCard(deckOfCard);
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //foreach (Card item in deckOfCard)
+            //{
+            //    Console.WriteLine("{0}", item);
+            //}
+
+            // პირველი ხელი /// 1 კარტის დარიგება თითო მოთამაშეზე
+            GamePlay play = new GamePlay(gamers, deckOfCard);
+            play.GameRound(9);
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            foreach (Gamer item in play._gamers)
             {
-                Console.WriteLine("{0}", item);
+                foreach (Card card in item.CardsOnHand)
+                {
+                    Console.WriteLine("Gamer: {0} - {1}", item._name, card.ToString());
+                }
             }
 
-            var mixDckOfCard = new MixDeckOfCard(deckOfCard);
-            Console.WriteLine("");
-            Console.WriteLine("");
-            foreach (Card item in deckOfCard)
-            {
-                Console.WriteLine("{0}", item);
-            }
         }
     }
 }
